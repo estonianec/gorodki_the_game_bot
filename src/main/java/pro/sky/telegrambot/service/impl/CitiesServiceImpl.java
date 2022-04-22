46eb29e05b5ba0604c5ab03b3fb820bd0bb0012b
estonec <rusecru@gmail.com>
2022-04-22 09:41:47 +0300

    Initial commit

--- src/main/java/pro/sky/telegrambot/service/impl/CitiesServiceImpl.java
+++ src/main/java/pro/sky/telegrambot/service/impl/CitiesServiceImpl.java
@@ -1,6 +1,8 @@
 package pro.sky.telegrambot.service.impl;
 
+import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.service.CitiesService;
 
+@Service
 public class CitiesServiceImpl implements CitiesService {
 }
